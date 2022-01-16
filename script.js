297d5a0467d981e8b3ed0c202e9917a65219b75b
Kevin Richard <kevinrichardve@gmail.com>
2022-01-16 20:56:12 -0300

    Correcao html

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const imagem = document.querySelector('img');
 const botao = document.querySelector('#botao');
+
+const imagem1 = document.querySelector('#img1');
 const nomeDoPersonagem1 = document.querySelector('#nome1');
 const especie1 = document.querySelector('#especie1');
 const condicao1 = document.querySelector('#condicao1');
 
+const imagem2 = document.querySelector('#img2');
 const nomeDoPersonagem2 = document.querySelector('#nome2');
 const especie2 = document.querySelector('#especie2');
 const condicao2 = document.querySelector('#condicao2');
 
+const imagem3 = document.querySelector('#img3');
 const nomeDoPersonagem3 = document.querySelector('#nome3');
 const especie3 = document.querySelector('#especie3');
 const condicao3 = document.querySelector('#condicao3');
@@ -15,39 +18,63 @@ const condicao3 = document.querySelector('#condicao3');
 
 
 gerarValorAletorio = () => {
-    return Math.floor(Math.random() *671);
+    return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAletorio = gerarValorAletorio();
+    let numeroAletorio1 = gerarValorAletorio();
     let numeroAletorio2 = gerarValorAletorio();
     let numeroAletorio3 = gerarValorAletorio();
 
-
-
-
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio1}`, {
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio1}`, {
         method: 'GET',
         headers: {
             Accept: 'application/jason',
             "Content-type": 'application/jason'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
         nomeDoPersonagem1.innerHTML = data.name;
         especie1.innerHTML = data.species;
         condicao1.innerHTML = data.status;
-        
-        
+
+    });
+
+
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio2}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/jason',
+            "Content-type": 'application/jason'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
         nomeDoPersonagem2.innerHTML = data.name;
         especie2.innerHTML = data.species;
         condicao2.innerHTML = data.status;
+    });
 
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio3}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/jason',
+            "Content-type": 'application/jason'
+        }
+    }).then((response) => response.json()).then((data) => {
+
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
         nomeDoPersonagem3.innerHTML = data.name;
         especie3.innerHTML = data.species;
         condicao3.innerHTML = data.status;
 
-    }); 
+    });
+
+
+
+
+
 }
 botao.onclick = pegarPersonagem;
