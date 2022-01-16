1c38d036d9cc2597ed0bf4e5fe2fc80d583a81ec
Kevin Richard <kevinrichardve@gmail.com>
2022-01-16 16:45:51 -0300

    implementar alterações

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('#botao');
-const nomeDoPersonagem = document.querySelector('#nome1');
-const especie = document.querySelector('#especie1');
-const condicao = document.querySelector('#condicao1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#condicao1');
+
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#condicao2');
+
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#condicao3');
+
+
 
 gerarValorAletorio = () => {
     return Math.floor(Math.random() *671);
@@ -10,7 +20,13 @@ gerarValorAletorio = () => {
 
 pegarPersonagem = () => {
     let numeroAletorio = gerarValorAletorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio}`, {
+    let numeroAletorio2 = gerarValorAletorio();
+    let numeroAletorio3 = gerarValorAletorio();
+
+
+
+
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAletorio1}`, {
         method: 'GET',
         headers: {
             Accept: 'application/jason',
@@ -19,9 +35,19 @@ pegarPersonagem = () => {
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = data.status; 
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML = data.status;
+        
+        
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = data.status;
+
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = data.status;
+
     }); 
 }
 botao.onclick = pegarPersonagem;
