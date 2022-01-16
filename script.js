51e00c0c4589ff70a23effcc5a7ac1dabaa253e1
Kevin Richard <kevinrichardve@gmail.com>
2022-01-16 01:23:46 -0300

    Capturar elementos html manipulaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome1');
+const especie = document.querySelector('#especie1');
+const condicao = document.querySelector('#condicao1');
+
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome2');
+const especie = document.querySelector('#especie2');
+const condicao = document.querySelector('#condicao2');
+
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome3');
+const especie = document.querySelector('#especie3');
+const condicao = document.querySelector('#condicao3');
+
